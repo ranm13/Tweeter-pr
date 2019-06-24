b83c5f3b22579851a045a93439ef697d28882530
Ran Marciano <ranm13@gmail.com>
2019-06-24 17:01:04 +0300

    first draft of logic.js

---
+++ logic.js
@@ -0,0 +1,96 @@
+const Tweeter = function(){
+    //Array of posts - private
+    const _posts = [
+        {
+            text: "First post!",
+            id: "p1",
+            comments: [
+                { id: "c1", text: "First comment on first post!" },
+                { id: "c2", text: "Second comment on first post!!" },
+                { id: "c3", text: "Third comment on first post!!!" }
+            ]
+        },
+        {
+            text: "Aw man, I wanted to be first",
+            id: "p2",
+            comments: [
+                { id: "c4", text: "Don't wory second poster, you'll be first one day." },
+                { id: "c5", text: "Yeah, believe in yourself!" },
+                { id: "c6", text: "Haha second place what a joke." }
+            ]
+        }
+    ]
+
+    //postIdCounter to count the number of total posts
+    let postIdCounter  = _posts.length
+
+    //commentIdCounter to count the number of total comments
+    let commentIdCounter = 6
+
+    //function that returns the posts array
+    const getPosts = () => _posts
+
+    //function that pushes new post into the posts array
+    const addPost = function(text){
+        const postID = "p" + (postIdCounter + 1)
+        postIdCounter += 1
+        return _posts.push({text: text, id: postID, comments: []})
+    }
+
+    //function that removes a post from the posts array by ID
+    const removePost = function(postID){
+        for(let index in _posts){
+            if (_posts[index].id === postID){
+                return _posts.splice(index, 1)
+            }
+        }
+    }
+
+    //function that pushes a text comment into the comments array of the post with the given ID
+    const addComment = function(text, postID){
+        const commentID = "c" + (commentIdCounter + 1)
+        commentIdCounter += 1
+        for(let index in _posts){
+            if (_posts[index].id === postID){
+                return _posts[index].comments.push({id: commentID, text: text})
+            }
+        }
+    }
+    
+
+    //function that removes a comment from the comments array of the specific post ID and comment ID
+    const removeComment = function(postID, commentID){
+        for(let index in _posts){
+            if (_posts[index].id === postID){
+                for(let commentIndex in _posts[index].comments){
+                    if(_posts[index].comments[commentIndex].id === commentID){
+                        return _posts[index].comments.splice(commentIndex, 1)
+                    }
+                }
+                
+            }
+        }
+    }
+
+    return {
+        getPosts,
+        addPost,
+        removePost,
+        addComment,
+        removeComment
+    }
+}
+
+
+
+const tweeter = Tweeter()
+tweeter.addPost("This is my own post!")
+tweeter.removePost("p1")
+tweeter.addComment("Damn straight it is!", "p3")
+tweeter.addComment("Second the best!", "p2")
+tweeter.removeComment("p2", "c6")
+console.log(tweeter.getPosts())
+
+
+
+
